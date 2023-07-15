e8b332140fd358e3cdc14ad34711a71fbc59e744
Samet CAN <cansamet1223@gmail.com>
2023-07-15 17:40:29 +0300

    Start Frontend Works

---
+++ FrontendWorks/Platforms/Tizen/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace FrontendWorks;
+
+class Program : MauiApplication
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	static void Main(string[] args)
+	{
+		var app = new Program();
+		app.Run(args);
+	}
+}
