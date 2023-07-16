108bea5d2a2e73fb2f0df6dc0f903de10030ec94
Samet CAN <cansamet1223@gmail.com>
2023-07-16 16:50:26 +0300

    Starting Login Page

---
+++ FrontendWorks/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace FrontendWorks;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FrontendWorks/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace FrontendWorks.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
