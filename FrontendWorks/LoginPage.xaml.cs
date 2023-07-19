00036f67735b4387065d2502c3c4d5243f96d7f9
Samet CAN <cansamet1223@gmail.com>
2023-07-19 18:11:41 +0300

    Just remain connection

--- FrontendWorks/LoginPage.xaml.cs
+++ FrontendWorks/LoginPage.xaml.cs
@@ -1,9 +1,12 @@
+using FrontendWorks.ViewModel;
+
 namespace FrontendWorks;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
+	public LoginPage(LoginPageViewModel loginPageViewModel)
 	{
 		InitializeComponent();
+		this.BindingContext = loginPageViewModel;
 	}
 }
