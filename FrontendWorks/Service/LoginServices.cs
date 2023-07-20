05a33f3cb3b34d6a6417ce3c01791684e9ce57e5
Samet CAN <cansamet1223@gmail.com>
2023-07-20 22:23:08 +0300

    Trying connect web api

--- FrontendWorks/Service/LoginServices.cs
+++ FrontendWorks/Service/LoginServices.cs
@@ -21,7 +21,10 @@ namespace FrontendWorks.Service
                     var userInfo = new UserInfo();
                     var client = new HttpClient();
 
-                    string url = "https://localhost:7083/api/First/Login/" + username + "/" + password;
+                    //string BaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "http://localhost:5000";
+                    //string TodoItemsUrl = $"{BaseAddress}/api/todoitems/";
+
+                    string url = "http://127.0.0.1:7083/api/First/Login/" + username + "/" + password;
                     client.BaseAddress = new Uri(url);
                     HttpResponseMessage response = await client.GetAsync("");
                     if (response.IsSuccessStatusCode)
