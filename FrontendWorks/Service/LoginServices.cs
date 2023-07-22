16096e46fc413319d7caf30e662e5dfecdf93650
Samet CAN <cansamet1223@gmail.com>
2023-07-22 23:20:18 +0300

    free progres

--- FrontendWorks/Service/LoginServices.cs
+++ FrontendWorks/Service/LoginServices.cs
@@ -21,12 +21,13 @@ namespace FrontendWorks.Service
                     var userInfo = new UserInfo();
                     var client = new HttpClient();
 
-                    //string BaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "http://localhost:5000";
-                    //string TodoItemsUrl = $"{BaseAddress}/api/todoitems/";
-
-                    string url = "http://127.0.0.1:7083/api/First/Login/" + username + "/" + password;
+                    string BaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "https://3ch9ls5d-5267.euw.devtunnels.ms" : "https://localhost:7083";
+                    //string BaseAddress = "https://3ch9ls5d-5267.euw.devtunnels.ms";
+                    string url = $"{BaseAddress}/api/First/Login/" + username + "/" + password;
                     client.BaseAddress = new Uri(url);
+                    //var ba = await client.GetAsync("");
                     HttpResponseMessage response = await client.GetAsync("");
+                    String a = response.Content.ToString();
                     if (response.IsSuccessStatusCode)
                     {
                         //string content = response.Content.ReadAsStringAsync().Result;
